5473727d69c97c8a5f3e0d338d0b46b55221548f
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2023-02-14 18:38:35 -0300

    exercicios-concluidos

--- src/components/ListaTarefas/index.js
+++ src/components/ListaTarefas/index.js
@@ -11,6 +11,8 @@ import {
 import bin from "../../assets/bin.png";
 
 export function ListaTarefas() {
+
+  const [lista, setLista] = useState(["Fazer exercícios", "Estudar React"]);
   const [novaTarefa, setNovaTarefa] = useState("");
 
 
@@ -18,9 +20,16 @@ export function ListaTarefas() {
     setNovaTarefa(event.target.value);
   };
 
-  const adicionaTarefa = () => {};
+  const adicionaTarefa = () => {
+    const novaLista = [...lista, novaTarefa];
+    setLista(novaLista);
+    setNovaTarefa("");
+  };
 
-  const removeTarefa = () => {};
+  const removeTarefa = (tarefa) => {
+    const listaFiltrada = lista.filter((item) => item !== tarefa);
+    setLista(listaFiltrada);
+  };
 
   return (
     <ListaTarefasContainer>
@@ -30,17 +39,21 @@ export function ListaTarefas() {
           value={novaTarefa}
           onChange={onChangeTarefa}
         />
-        <AddTaskButton>Adicionar</AddTaskButton>
+        <AddTaskButton onClick={adicionaTarefa}>Adicionar</AddTaskButton>
       </InputContainer>
 
       <ListaContainer>
-        <ul>
-          <Tarefa>
-            <p>Nova tarefa</p>
-            <RemoveButton>
-              <img src={bin} alt="" width="16px" />
-            </RemoveButton>
-          </Tarefa>
+      <ul>
+          {lista.map((tarefa, index) => {
+            return (
+              <Tarefa key={index}>
+                <p>{tarefa}</p>
+                <RemoveButton onClick={() => removeTarefa(tarefa)}>
+                  <img src={bin} alt="" width="16px" />
+                </RemoveButton>
+              </Tarefa>
+            );
+          })}
         </ul>
       </ListaContainer>
     </ListaTarefasContainer>
